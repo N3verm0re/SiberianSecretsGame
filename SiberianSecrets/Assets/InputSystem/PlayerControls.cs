dd5345efbec5da7b5112a7b72bd1757f5387f4b2
Bruno Gomes <bruno.gomes.771@outlook.com>
2020-11-18 04:18:07 +0000

    Version 0.02.0

--- SiberianSecrets/Assets/InputSystem/PlayerControls.cs
+++ SiberianSecrets/Assets/InputSystem/PlayerControls.cs
@@ -34,19 +34,161 @@ public class @PlayerControls : IInputActionCollection, IDisposable
                     ""path"": ""<Gamepad>/leftStick"",
                     ""interactions"": """",
                     ""processors"": """",
-                    ""groups"": """",
+                    ""groups"": ""Gamepad"",
                     ""action"": ""Move"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
+                }
+            ]
+        },
+        {
+            ""name"": ""MenuNavigation"",
+            ""id"": ""08e596ff-ca07-4eeb-b232-d937593c445a"",
+            ""actions"": [
+                {
+                    ""name"": ""Navigate"",
+                    ""type"": ""Value"",
+                    ""id"": ""5edf7904-e9e9-41d8-ac14-aa065fb4fdf0"",
+                    ""expectedControlType"": ""Dpad"",
+                    ""processors"": """",
+                    ""interactions"": """"
+                },
+                {
+                    ""name"": ""Select"",
+                    ""type"": ""Button"",
+                    ""id"": ""56530162-c22a-4453-8452-f8cef4a170bb"",
+                    ""expectedControlType"": ""Button"",
+                    ""processors"": """",
+                    ""interactions"": """"
+                },
+                {
+                    ""name"": ""Cancel"",
+                    ""type"": ""Button"",
+                    ""id"": ""2806aa83-2ea3-4296-ae5d-2eac6c3ef375"",
+                    ""expectedControlType"": ""Button"",
+                    ""processors"": """",
+                    ""interactions"": """"
+                }
+            ],
+            ""bindings"": [
+                {
+                    ""name"": ""2D Vector"",
+                    ""id"": ""0ccec7d2-8a8e-4d42-98c7-2660b31706df"",
+                    ""path"": ""2DVector"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Navigate"",
+                    ""isComposite"": true,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": ""up"",
+                    ""id"": ""a1699725-26e7-4df1-8d6e-6cc10ffdf2a7"",
+                    ""path"": ""<Gamepad>/dpad/up"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": ""Gamepad"",
+                    ""action"": ""Navigate"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": ""down"",
+                    ""id"": ""7eea377a-53d6-4f83-b0d0-3bc12cdc915a"",
+                    ""path"": ""<Gamepad>/dpad/down"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": ""Gamepad"",
+                    ""action"": ""Navigate"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": ""left"",
+                    ""id"": ""ad7f5382-625b-4da2-adcc-56324325c97e"",
+                    ""path"": ""<Gamepad>/dpad/left"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": ""Gamepad"",
+                    ""action"": ""Navigate"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": ""right"",
+                    ""id"": ""f5f74c1c-449d-4e76-9ded-52c9ad4cee72"",
+                    ""path"": ""<Gamepad>/dpad/right"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": ""Gamepad"",
+                    ""action"": ""Navigate"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": """",
+                    ""id"": ""ce1f1270-847d-4477-bdcd-2b036f592fd8"",
+                    ""path"": ""<Gamepad>/buttonSouth"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": ""Gamepad"",
+                    ""action"": ""Select"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": """",
+                    ""id"": ""80124a9b-11da-4418-a832-2fb9d36bbb54"",
+                    ""path"": ""<Gamepad>/buttonEast"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": ""Gamepad"",
+                    ""action"": ""Cancel"",
                     ""isComposite"": false,
                     ""isPartOfComposite"": false
                 }
             ]
         }
     ],
-    ""controlSchemes"": []
+    ""controlSchemes"": [
+        {
+            ""name"": ""Gamepad"",
+            ""bindingGroup"": ""Gamepad"",
+            ""devices"": [
+                {
+                    ""devicePath"": ""<Gamepad>"",
+                    ""isOptional"": false,
+                    ""isOR"": false
+                }
+            ]
+        },
+        {
+            ""name"": ""Keyboard & Mouse"",
+            ""bindingGroup"": ""Keyboard & Mouse"",
+            ""devices"": [
+                {
+                    ""devicePath"": ""<Keyboard>"",
+                    ""isOptional"": false,
+                    ""isOR"": false
+                },
+                {
+                    ""devicePath"": ""<Mouse>"",
+                    ""isOptional"": false,
+                    ""isOR"": false
+                }
+            ]
+        }
+    ]
 }");
         // Gameplay
         m_Gameplay = asset.FindActionMap("Gameplay", throwIfNotFound: true);
         m_Gameplay_Move = m_Gameplay.FindAction("Move", throwIfNotFound: true);
+        // MenuNavigation
+        m_MenuNavigation = asset.FindActionMap("MenuNavigation", throwIfNotFound: true);
+        m_MenuNavigation_Navigate = m_MenuNavigation.FindAction("Navigate", throwIfNotFound: true);
+        m_MenuNavigation_Select = m_MenuNavigation.FindAction("Select", throwIfNotFound: true);
+        m_MenuNavigation_Cancel = m_MenuNavigation.FindAction("Cancel", throwIfNotFound: true);
     }
 
     public void Dispose()
@@ -125,8 +267,81 @@ public class @PlayerControls : IInputActionCollection, IDisposable
         }
     }
     public GameplayActions @Gameplay => new GameplayActions(this);
+
+    // MenuNavigation
+    private readonly InputActionMap m_MenuNavigation;
+    private IMenuNavigationActions m_MenuNavigationActionsCallbackInterface;
+    private readonly InputAction m_MenuNavigation_Navigate;
+    private readonly InputAction m_MenuNavigation_Select;
+    private readonly InputAction m_MenuNavigation_Cancel;
+    public struct MenuNavigationActions
+    {
+        private @PlayerControls m_Wrapper;
+        public MenuNavigationActions(@PlayerControls wrapper) { m_Wrapper = wrapper; }
+        public InputAction @Navigate => m_Wrapper.m_MenuNavigation_Navigate;
+        public InputAction @Select => m_Wrapper.m_MenuNavigation_Select;
+        public InputAction @Cancel => m_Wrapper.m_MenuNavigation_Cancel;
+        public InputActionMap Get() { return m_Wrapper.m_MenuNavigation; }
+        public void Enable() { Get().Enable(); }
+        public void Disable() { Get().Disable(); }
+        public bool enabled => Get().enabled;
+        public static implicit operator InputActionMap(MenuNavigationActions set) { return set.Get(); }
+        public void SetCallbacks(IMenuNavigationActions instance)
+        {
+            if (m_Wrapper.m_MenuNavigationActionsCallbackInterface != null)
+            {
+                @Navigate.started -= m_Wrapper.m_MenuNavigationActionsCallbackInterface.OnNavigate;
+                @Navigate.performed -= m_Wrapper.m_MenuNavigationActionsCallbackInterface.OnNavigate;
+                @Navigate.canceled -= m_Wrapper.m_MenuNavigationActionsCallbackInterface.OnNavigate;
+                @Select.started -= m_Wrapper.m_MenuNavigationActionsCallbackInterface.OnSelect;
+                @Select.performed -= m_Wrapper.m_MenuNavigationActionsCallbackInterface.OnSelect;
+                @Select.canceled -= m_Wrapper.m_MenuNavigationActionsCallbackInterface.OnSelect;
+                @Cancel.started -= m_Wrapper.m_MenuNavigationActionsCallbackInterface.OnCancel;
+                @Cancel.performed -= m_Wrapper.m_MenuNavigationActionsCallbackInterface.OnCancel;
+                @Cancel.canceled -= m_Wrapper.m_MenuNavigationActionsCallbackInterface.OnCancel;
+            }
+            m_Wrapper.m_MenuNavigationActionsCallbackInterface = instance;
+            if (instance != null)
+            {
+                @Navigate.started += instance.OnNavigate;
+                @Navigate.performed += instance.OnNavigate;
+                @Navigate.canceled += instance.OnNavigate;
+                @Select.started += instance.OnSelect;
+                @Select.performed += instance.OnSelect;
+                @Select.canceled += instance.OnSelect;
+                @Cancel.started += instance.OnCancel;
+                @Cancel.performed += instance.OnCancel;
+                @Cancel.canceled += instance.OnCancel;
+            }
+        }
+    }
+    public MenuNavigationActions @MenuNavigation => new MenuNavigationActions(this);
+    private int m_GamepadSchemeIndex = -1;
+    public InputControlScheme GamepadScheme
+    {
+        get
+        {
+            if (m_GamepadSchemeIndex == -1) m_GamepadSchemeIndex = asset.FindControlSchemeIndex("Gamepad");
+            return asset.controlSchemes[m_GamepadSchemeIndex];
+        }
+    }
+    private int m_KeyboardMouseSchemeIndex = -1;
+    public InputControlScheme KeyboardMouseScheme
+    {
+        get
+        {
+            if (m_KeyboardMouseSchemeIndex == -1) m_KeyboardMouseSchemeIndex = asset.FindControlSchemeIndex("Keyboard & Mouse");
+            return asset.controlSchemes[m_KeyboardMouseSchemeIndex];
+        }
+    }
     public interface IGameplayActions
     {
         void OnMove(InputAction.CallbackContext context);
+    }
+    public interface IMenuNavigationActions
+    {
+        void OnNavigate(InputAction.CallbackContext context);
+        void OnSelect(InputAction.CallbackContext context);
+        void OnCancel(InputAction.CallbackContext context);
     }
 }
---
+++ SiberianSecrets/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.InputSystem.Controls;
+using UnityEngine.UI;
+
+public class MainMenu : MonoBehaviour
+{
+    PlayerControls controls;
+
+    GameObject currentActivePanel;
+    public GameObject menuPanel, settingsPanel, tutorialPanel;
+    public GameObject menuFirstButton, settingsFirstButton, tutorialFirstButton;
+    Button[] menuButtonList, settingsButtonList, tutorialButtonList;
+    int menuButtonIndex = 0, settingsButtonIndex = 0, tutorialButtonIndex = 0;
+
+    private void Awake()
+    {
+        currentActivePanel = menuPanel;
+        menuPanel.SetActive(true);
+        settingsPanel.SetActive(false);
+        tutorialPanel.SetActive(false);
+
+        controls = new PlayerControls();
+        //Maybe put these lists as individual scripts on each panel? Easier to use and can manually add each option to avoid navigation bugs
+        menuButtonList = menuPanel.GetComponentsInChildren<Button>();
+        settingsButtonList = settingsPanel.GetComponentsInChildren<Button>();
+        tutorialButtonList = tutorialPanel.GetComponentsInChildren<Button>();
+
+        //NavigationInput
+        controls.MenuNavigation.Navigate.performed += ctx => navigateButtonIndexes(ctx.ReadValue<Vector2>());
+        controls.MenuNavigation.Select.performed += ctx => PressButton();
+    }
+
+    private void Update()
+    {
+        
+    }
+
+    public void navigateButtonIndexes(Vector2 navigation)
+    {
+        Debug.Log($"DpadInput: {navigation.x}, {navigation.y}");
+
+        if (currentActivePanel == menuPanel)
+        {
+            if(menuButtonIndex < menuButtonList.Length && navigation.x > 0)
+            {
+                menuButtonIndex++;
+            }
+            else if (menuButtonIndex > 0 && navigation.y > 0)
+            {
+                menuButtonIndex--;
+            }
+
+            EventSystem.current.SetSelectedGameObject(menuButtonList[menuButtonIndex].gameObject);
+        }
+        else if (currentActivePanel == settingsPanel)
+        {
+            if (settingsButtonIndex < settingsButtonList.Length && navigation.x > 0)
+            {
+                settingsButtonIndex++;
+            }
+            else if (settingsButtonIndex > 0 && navigation.y > 0)
+            {
+                settingsButtonIndex--;
+            }
+
+            EventSystem.current.SetSelectedGameObject(settingsButtonList[settingsButtonIndex].gameObject);
+        }
+        else if (currentActivePanel == tutorialPanel)
+        {
+            if (tutorialButtonIndex < tutorialButtonList.Length && navigation.x > 0)
+            {
+                tutorialButtonIndex++;
+            }
+            else if (tutorialButtonIndex > 0 && navigation.y > 0)
+            {
+                tutorialButtonIndex = 0;
+            }
+
+            EventSystem.current.SetSelectedGameObject(tutorialButtonList[tutorialButtonIndex].gameObject);
+        }
+    }
+
+    public void PressButton()
+    {
+        Button currentButton = EventSystem.current.currentSelectedGameObject.GetComponent<Button>();
+        currentButton.onClick.Invoke();
+    }
+}
